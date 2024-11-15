75d2cb95cfc1b524c72e8eea532fe87aed502492
kenzokrizz <kenechukwu029@revature.net>
2024-11-15 01:00:21 -0500

    changes made

--- Guesser/Analyzer.sh
+++ Guesser/Analyzer.sh
@@ -3,8 +3,8 @@
 # This is my Analyzer.
 # When run, it performs an analysis of the Data.txt file.
 
-GAMEDATA="./Gamedata.txt"
-STATISTICS="./Statistics.txt"
+GAMEDATA="/mnt/c/Revature/keneE/guesser/Gamedata.txt"
+STATISTICS="/mnt/c/Revature/keneE/guesser/Statistics.txt"
 
 # Checks to see if data file exist and if not create new file
  if [[ ! -f "$STATISTICS" ]]; then
@@ -12,7 +12,7 @@ STATISTICS="./Statistics.txt"
     touch "$STATISTICS"
  fi
 
-LIB="./Library.sh"
+LIB="/mnt/c/Revature/keneE/guesser/Library.sh"
 if [[ -e $LIB ]]; then
     source "$LIB"
 else
